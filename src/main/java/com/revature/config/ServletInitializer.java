7909e2c73db56d98955aaa2cdcdd1c10a9343cc3
James Scarnati <89417758+Xerast@users.noreply.github.com>
2021-10-26 17:30:47 -0500

    Add files via upload

---
+++ src/main/java/com/revature/config/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.revature.config;
+
+import com.revature.foodinstagram.FoodInstagramApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+    return application.sources(FoodInstagramApplication.class);
+}
+}
